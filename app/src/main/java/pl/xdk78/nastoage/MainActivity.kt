07a5623efa5cae069912d5814ab486155ddf0e2e
xdk78 <xdk78@interia.pl>
2017-07-23 22:19:40 +0200

    Small cleanup

--- app/src/main/java/pl/xdk78/nastoage/MainActivity.kt
+++ app/src/main/java/pl/xdk78/nastoage/MainActivity.kt
@@ -1,7 +1,7 @@
 package pl.xdk78.nastoage
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
@@ -14,11 +14,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -28,9 +23,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
