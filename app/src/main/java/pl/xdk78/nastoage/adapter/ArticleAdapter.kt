67ffa6a236a0961cb23ec208d91166d5aeed77d5
xdk78 <xdk78@interia.pl>
2017-12-10 18:53:08 +0100

    Fix images inside Html and update dependencies

--- app/src/main/java/pl/xdk78/nastoage/adapter/ArticleAdapter.kt
+++ app/src/main/java/pl/xdk78/nastoage/adapter/ArticleAdapter.kt
@@ -1,30 +1,42 @@
 package pl.xdk78.nastoage.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.drawable.LevelListDrawable
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.RecyclerView
 import android.text.Html
+import android.text.Spanned
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import org.jetbrains.anko.find
 import pl.xdk78.nastoage.R
+import pl.xdk78.nastoage.helper.ImageGetterAsyncTask
 import pl.xdk78.nastoage.model.Article
 
 /**
  * Created by xdk78 on 2017-07-28.
  */
-class ArticleAdapter(var context: Context, var articles: List<Article>) : RecyclerView.Adapter<ArticleAdapter.ArticleAdapterViewHolder>() {
+class ArticleAdapter(private var context: Context, private var articles: List<Article>) : RecyclerView.Adapter<ArticleAdapter.ArticleAdapterViewHolder>() {
 
-
-    init {
-        this.context = context
-    }
-
+    @SuppressLint("PrivateResource")
     override fun onBindViewHolder(holder: ArticleAdapterViewHolder, position: Int) {
         val item = articles[position]
         holder.title.text = Html.fromHtml(item.title.rendered)
-        holder.content.text = Html.fromHtml(item.excerpt.rendered)
+
+        val spanned: Spanned
+        spanned = Html.fromHtml(item.content.rendered,
+                Html.ImageGetter { source ->
+                    val d = LevelListDrawable()
+                    val empty = ContextCompat.getDrawable(context, R.drawable.abc_btn_check_material)
+                    d.addLevel(0, 0, empty)
+                    d.setBounds(0, 0, empty.intrinsicWidth, empty.intrinsicHeight)
+                    ImageGetterAsyncTask(context, source, d).execute(holder.content)
+                    d
+                }, null)
+        holder.content.text = spanned
 
         /*Glide.with(context)
                 .asDrawable()
@@ -43,8 +55,8 @@ class ArticleAdapter(var context: Context, var articles: List<Article>) : Recycl
     }
 
     class ArticleAdapterViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val title: TextView = view.find<TextView>(R.id.title)
-        val content: TextView = view.find<TextView>(R.id.content)
+        val title: TextView = view.find(R.id.title)
+        val content: TextView = view.find(R.id.content)
         // val image: ImageView = view.find<ImageView>(R.id.main_image)
     }
 }
---
+++ app/src/main/java/pl/xdk78/nastoage/helper/ImageGetterAsyncTask.kt
@@ -0,0 +1,48 @@
+package pl.xdk78.nastoage.helper
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Point
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.LevelListDrawable
+import android.os.AsyncTask
+import android.util.Log
+import android.widget.TextView
+import com.bumptech.glide.Glide
+
+
+class ImageGetterAsyncTask(@SuppressLint("StaticFieldLeak") private val context: Context, private val source: String, private val levelListDrawable: LevelListDrawable) : AsyncTask<TextView, Void, Bitmap>() {
+    @SuppressLint("StaticFieldLeak")
+    private var t: TextView? = null
+    private val TAG = "IMG"
+    override fun doInBackground(vararg params: TextView): Bitmap? {
+        t = params[0]
+        try {
+            Log.d(TAG, "Downloading the image from: " + source)
+            return Glide.with(context).asBitmap().load(source).submit().get()
+        } catch (e: Exception) {
+            return null
+        }
+
+    }
+
+    override fun onPostExecute(bitmap: Bitmap) {
+        try {
+            val d = BitmapDrawable(context.resources, bitmap)
+            val size = Point()
+            (context as Activity).windowManager.defaultDisplay.getSize(size)
+            // Lets calculate the ratio according to the screen width in px
+            val multiplier = size.x / bitmap.width
+            Log.d(TAG, "multiplier: " + multiplier)
+            levelListDrawable.addLevel(1, 1, d)
+            // Set bounds width  and height according to the bitmap resized size
+            levelListDrawable.setBounds(0, 0, bitmap.width * multiplier, bitmap.height * multiplier)
+            levelListDrawable.level = 1
+            t!!.text = t!!.text // invalidate() doesn't work correctly...
+        } catch (e: Exception) { /* Like a null bitmap, etc. */
+        }
+
+    }
+}
